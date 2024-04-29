84bcb3ed1d60e2f421e8fb3f90aecae5b70a8134
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-04-30 00:47:21 +0300

    Fix names

--- dnevnik365/backend/api/settings.py
+++ dnevnik365/backend/api/settings.py
@@ -4,8 +4,8 @@ from os import environ
 
 load_dotenv()
 
-DB_USERNAME = environ.get('DB_USERNAME', 'postgres')
-DB_PASSWORD = environ.get('DB_PASSWORD', 'postgres')
-DB_HOST = environ.get('DB_HOST', 'localhost')
-DB_PORT = int(environ.get('DB_PORT'))
+DB_USERNAME = environ.get('POSTGRES_USERNAME', 'postgres')
+DB_PASSWORD = environ.get('POSTGRES_PASSWORD', 'postgres')
+DB_HOST = environ.get('POSTGRES_HOST', 'localhost')
+DB_PORT = int(environ.get('POSTGRES_PORT', 5432))
 DATABASE_URL = f'postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}'
