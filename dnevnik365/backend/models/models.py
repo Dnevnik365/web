bb337a2f765aa0064b4e3a0a228dbbf2123133e1
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-05-01 00:48:45 +0300

    Update models

--- dnevnik365/backend/models/models.py
+++ dnevnik365/backend/models/models.py
@@ -1,28 +1,43 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from pydantic import BaseModel
+
+from typing import Optional, Dict, List
+from datetime import date
 
 
 metadata = MetaData()
 
-users = Table(
-    'users',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('username', String, nullable=True),
-    Column('password', String, nullable=True),
-    Column('role', String, nullable=False),
-    Column('service', String, nullable=False)
-)
 
-purposes = Table(
-    'purposes',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('purposes', nullable=True)
-)
+class Base(DeclarativeBase):
+    pass
 
-timetable = Table(
-    'timetable',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('timetable')
-)
+
+class User(BaseModel):
+    id: int = None
+    name: Optional[str] = None
+    login: str
+    password: str
+    role: str
+    service: str
+
+
+class UserModel(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[Optional[str]] = mapped_column(nullable=True)
+    login: Mapped[str]
+    password: Mapped[str]
+    role: Mapped[str]
+    service: Mapped[str]
+
+
+class Puproses(BaseModel):
+    user_id: int
+    purposes: Dict[str, float] = {}
+
+
+class Timetable(BaseModel):
+    user_id: int
+    timetable: Dict[date, List[str]] = {}
