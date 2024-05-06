210cee7adddcaa1399f960948743113fbf35e0c8
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-05-06 20:43:48 +0300

    Add endpoints

---
+++ dnevnik365/backend/api/timetable.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from datetime import date
+from typing import List, Dict
+
+from dnevnik365.backend.scripts.base import BaseStudentDnevnik
+
+
+timetable_router = APIRouter(prefix='timetable')
+
+
+@timetable_router.post('/')
+async def get_timetable(dnevnik_obj: BaseStudentDnevnik, period: List[date] = None) -> Dict[date, List[str]]:
+    return await dnevnik_obj.get_school_timetable(period=period)
