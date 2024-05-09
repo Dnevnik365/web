35239a6fb3e7d4bcc1b0fad8ba9abb7957ca175d
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-05-09 16:43:14 +0300

    Add PurposeValue class

--- dnevnik365/backend/models/pydantic_models.py
+++ dnevnik365/backend/models/pydantic_models.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from typing import Optional, Dict, List
+from typing import Optional, Dict
 from datetime import date
 from enum import Enum
 
@@ -10,6 +10,15 @@ class Service(Enum):
     ballovnet = 'ballovnet'
     eljur = 'eljur'
     kirov_education = 'kirov_education'
+
+
+class PurposeValue(Enum):
+    _2_5 = 2.5
+    _3 = 3.0
+    _3_5 = 3.5
+    _4 = 4.0
+    _4_5 = 4.5
+    _5 = 5.0
 
 
 class Role(Enum):
@@ -28,7 +37,7 @@ class User(BaseModel):
 
 class Puproses(BaseModel):
     user_id: int
-    purposes: Dict[str, float] = {}
+    purposes: Dict[str, PurposeValue] = {}
 
 
 class Homework(BaseModel):
